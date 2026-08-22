4a70d45db39240f4af036bdd31e2a11e22cc0a7e
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-22 12:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (57.88%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i = 0
+    let j = 0
+    let k = nums.length -1
+    while(i<=k){
+        if(nums[i] == 0){
+            let temp = nums[i]
+            nums[i] = nums[j]
+            nums[j]= temp
+            j++
+            i++
+        }else if(nums[i] == 2){
+            let temp = nums[i]
+            nums[i] = nums[k]
+            nums[k]= temp
+            k--
+        }else i++
+        
+    }
+};
