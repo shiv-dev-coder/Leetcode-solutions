12ddb6b1308c797656f5e64e095a1a58e57bd72d
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-22 17:27:16 +0530

    Time: 36 ms (59.67%) | Memory: 70 MB (97.89%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let ans = []
+    nums.sort((a,b)=> a-b)
+    for(let i =0; i< nums.length; i++){
+        if(i!=0 && nums[i] == nums[i-1]) continue
+        let j = i+1, k = nums.length-1
+        while(j<k){
+            let sum = nums[i] + nums[j] + nums[k]
+            if(sum == 0){
+                let temp = [nums[i], nums[j++], nums[k--]]
+                ans.push(temp)
+                while(j<k && nums[j] == nums[j-1]) j++
+                while(j<k && nums[k] == nums[k+1]) k--
+            }else if(sum<0) j++
+            else k--
+        }
+
+    }
+    return ans
+};
