44338009840f16c9e956f558c9e5ce13caf8060e
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-22 21:38:18 +0530

    Time: 1 ms (60.81%) | Memory: 54.7 MB (39.57%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let str = ""
+    for(let i = 0; i< strs[0].length; i++){
+        let ref = strs[0][i]
+        let count = 0
+        for(let j = 0; j< strs.length; j++){
+           if(i>=strs[j].length || strs[j][i] != ref) return str
+        }
+        str+=ref
+    }
+    return str
+};
