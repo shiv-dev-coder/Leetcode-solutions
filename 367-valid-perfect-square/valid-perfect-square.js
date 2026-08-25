095d6f1da3ad73018f9a550b61caa7eac0db6eab
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-25 21:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (50.57%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.js
+++ 367-valid-perfect-square/valid-perfect-square.js
@@ -3,8 +3,12 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    for(let i = 0; i*i<= num; i++){
-        if((i*i) == num) return true
-    }
-    return false
+   let i = 0, j = num
+   while(i<=j){
+    let mid = Math.floor((i+j)/2)
+    if((mid*mid) == num) return true
+    else if((mid*mid)>num) j= mid -1
+    else i = mid+1
+   }
+   return false
 };
