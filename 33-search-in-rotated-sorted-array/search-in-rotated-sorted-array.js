43e6e0a58425611975f615be3e4814981fb3e1bd
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-31 10:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (80.08%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l= 0, r = nums.length-1
+    while(l<=r){
+        let mid = Math.floor(l+(r-l)/2)
+        if(nums[mid] == target) return mid
+        if(nums[l] <=nums[mid]){
+            if(target >= nums[l] && target < nums[mid]){
+                r = mid-1
+            }else l = mid+1
+        }else{
+            if(target > nums[mid] && target <= nums[r]){
+                l = mid+1
+            }else r = mid-1
+
+        }
+
+    }
+    return -1
+};
