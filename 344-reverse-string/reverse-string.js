45a2e8810864d8829944b63fdf95db025254bb0c
shiv chaudhari <shivchaudhari866@gmail.com>
2026-08-31 13:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 61.8 MB (31.22%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -5,7 +5,9 @@
 var reverseString = function(s) {
     let i =0, j = s.length-1
     while(i<=j){
-        [s[i], s[j]] = [s[j], s[i]]
+        let temp = s[i]
+        s[i] = s[j]
+        s[j] = temp 
         i++
         j--
     }
